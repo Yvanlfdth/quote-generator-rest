41adc75b26ffc2ed877c1e91191688fbf8df7110
Yvanlfdth <yvan.lefeveredetenhove@gmail.com>
2024-10-27 12:26:25 +0100

    added SERVER to .env

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ npm run dev
 
 - The first time you run the app, you should run the following route to populate the database for authors, quotes and tags collections (replace .env variables with your own data):
 ```
-http://${DATABASE_SERVER}:{PORT}/setup/db/init
+http://${SERVER}:{PORT}/setup/db/init
 ```
 
 # Project Structure
--- server.ts
+++ server.ts
@@ -3,9 +3,10 @@ import Server from "@src/index";
 
 const app: Application = express();
 const server: Server = new Server(app);
+const SERVER_NAME: string = process.env.SERVER ? process.env.SERVER : "localhost"; // get server from .env, otherwise inits it at 80 (http)
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 80; // get port from .env, otherwise inits it at 80 (http)
 
-app.listen(PORT, "localhost", function () {
+app.listen(PORT, SERVER_NAME, function () {
     console.log(`Server is running on port ${PORT}.`);
 })
 .on("error", (err: any) => {
